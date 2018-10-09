cc17739d1657312b46323908171585a5df2117bd
Colin Wilson <colin.p.wilson@marks-and-spencer.com>
2018-10-09 15:19:59 +0100

    Initial Commit

---
+++ ContentWhisperer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ContentWhisperer
+//
+//  Created by Colin Wilson on 09/10/2018.
+//  Copyright © 2018 Colin Wilson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
